dbd6370f8425a02681398bbbd0884a1013b410b0
hiroki8730 <funassynassyyy@ezweb.ne.jp>
2021-06-24 00:29:50 +0900

    正答数が表示出来たらおわり

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ let choices = [];
 let check = [];
 let buttonClickCount = 0;
 let title = document.getElementById("title");
-// ---取得した問題--- //
+// ---取得した問題--- //s
 let contents = [];
-// --- 0~3までの乱数作成 --- //
-let min = 0;
-let max = 3;
 // --- 合っていた答えの数 --- //
 let correctAns = 0;
 let randomNumber = 0;
 const questionItems = [];
+const btns = document.getElementsByClassName("btn");
+const select = document.getElementById("select");
+let removedQuestion = [];
 
 $quiz.textContent = "以下のボタンをクリック";
 
@@ -40,6 +40,7 @@ const callApi = () => {
             $startBtn.style.display = "none";
             contents = data.results;
             // contentsの中に選択肢をpushする
+            console.log(contents);
             contents.forEach(function (content) {
                 const tempAnswers = [];
                 tempAnswers.push({ isCorrect: true, text: content.correct_answer });
@@ -48,8 +49,9 @@ const callApi = () => {
                 tempAnswers.push({ isCorrect: false, text: content.incorrect_answers[2] });
                 questionItems.push(tempAnswers);
             });
-            console.log(questionItems);
-            console.log(contents);
+            // console.log(questionItems);
+            console.log(contents.slice(0, 1));
+            removedQuestion = contents.slice(0, 1);
             showQuestion();
             // dataContents.forEach(function(content){
             //     console.log(content);
@@ -61,66 +63,118 @@ const callApi = () => {
 };
 
 function showQuestion() {
-    for(let i = 0; i < buttonClickCount; i++){
+    select.style.display = "block";
+    select.style.listStyle = "none";
+    // for(let i = 0; i < removedQuestion.length; i++){
+    let i = 0;
+    if(i < removedQuestion.length){
     title.textContent = "";
     $genre.innerText = "[ジャンル]" + contents[i].category;
     $difficult.innerText = "[難易度]" + contents[i].difficulty;
     $quiz.innerText = contents[i].question;
-    choices.push(contents[i].correct_answer, contents[i].incorrect_answers[0], contents[i].incorrect_answers[1], contents[i].incorrect_answers[2])
-    title.textContent = "問題" + i;
-    btnCreate();
+    // choices.push(contents[i].correct_answer, contents[i].incorrect_answers[0], contents[i].incorrect_answers[1], contents[i].incorrect_answers[2])
+    title.textContent = "問題" + buttonClickCount;
+    btnCreate(questionItems[i]);
+    console.log(questionItems);
+    // };
+    } else {
+        title.textContent = "あなたの正当数は";
+        $genre.innerText = "";
+        $difficult.innerText = "";
+        $quiz.innerText = "再度チャレンジしたい場合は以下のボタンをクリック!!";
+        select.style.display = "none";
+        const homeBtn = document.createElement("button");
+        document.getElementById("home-button-container").appendChild(homeBtn);
+        homeBtn.innerText = "ホームに戻る";
+        homeBtn.addEventListener("click", function() {
+            location.reload();
+        })
     }
-}
+};
 
 // --- 選択肢のボタンを作成 --- //
-function btnCreate() {
-    for (let i = min; i <= max; i++) {
-        const startBtn = document.createElement("button");
-        ansBtn = startBtn;
-        ansBtn.id = "answerBtn";
-        console.log(ansBtn);
-        $answers.appendChild(ansBtn);
-        intRandomShuffle(ansBtn);
-        ansBtn.addEventListener("click", function () {
-            if (ansBtn.textContent === choices[i]) {
-                correctAns++;
-                // console.log(correctAns);
-                answerCheck(ansBtn);
-            } else {
-                answerCheck(ansBtn);
-                // console.log(correctAns);
-            }
-        }
-        );
-    }
-};
+function btnCreate(qItem) {
+    buttonClickCount++;
+    let num = qItem.length;
+    while (num) {
+        let tmp = Math.floor(Math.random() * num);
+        let str = qItem[--num];
+        qItem[num] = qItem[tmp];
+        qItem[tmp] = str;
 
-// --- 0~3までの乱数作成 --- //
-function intRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-// --- 0~3までの乱数をシャッフルする --- //
-function intRandomShuffle(ansBtn) {
-    while (true) {
-        let tmp = intRandom(min, max);
-        randomNumber = tmp;
-        if (!check.includes(randomNumber)) {
-            check.push(randomNumber);
-            ansBtn.innerText = choices[randomNumber];
-            console.log(randomNumber);
-            break;
+        for (let i = 0; i < qItem.length; i++) {
+            btns[i].textContent = qItem[i].text;
+            // console.log(btns[i].textContent); =>選択肢4つ x 4
         }
     }
-}
+    console.log(qItem);
+};
+// ボタンクリックされた時の処理
+function pushChoices(event) {
+    removedQuestion = [];
+    questionItems.shift();
+    contents.shift();
+    removedQuestion = contents.slice(0, 1);
+    showQuestion();
+};
+
 
 // ---選択肢を押した時の動作--- //
 function answerCheck() {
     i++;
-    title.textContent = "問題" + i;
+    // title.textContent = "問題" + i;
     $genre.innerText = "[ジャンル]" + dataContents[i].category;
     $difficult.innerText = "[難易度]" + dataContents[i].difficulty;
     $quiz.innerText = dataContents[i].question;
     choices = [];
     choices.push(dataContents[i].correct_answer, dataContents[i].incorrect_answers[0], dataContents[i].incorrect_answers[1], dataContents[i].incorrect_answers[2]);
 };
+
+// // shiftメソッドテスト
+// let arr = ["apple", "banana", "waterMelon", "orange" ];
+// arr.shift();
+// console.log(arr.shift()); // => apple
+// console.log(arr); // =>["banana", "waterMelon", "orange""]
+// console.log(arr.length); // => 3
+
+// --- 0~3までの乱数をシャッフルする --- //
+// function intRandomShuffle(ansBtn) {
+//     while (true) {
+//         let tmp = Math.floor(Math.random() * 4);
+//         randomNumber = tmp;
+//         if (!check.includes(randomNumber)) {
+//             check.push(randomNumber);
+//             ansBtn.innerText = choices[randomNumber];
+//             console.log(randomNumber);
+//             break;
+//         }
+//     }
+// }
+
+    // for (let i = min; i <= max; i++) {
+    //     const startBtn = document.createElement("button");
+    //     ansBtn = startBtn;
+    //     ansBtn.id = "answerBtn";
+    //     console.log(ansBtn);
+    //     $answers.appendChild(ansBtn);
+    //     intRandomShuffle(ansBtn);
+    //     ansBtn.addEventListener("click", function () {
+    //         if (ansBtn.textContent === choices[i]) {
+    //             correctAns++;
+    //             // console.log(correctAns);
+    //             answerCheck(ansBtn);
+    //         } else {
+    //             answerCheck(ansBtn);
+    //             // console.log(correctAns);
+    //         }
+    //     }
+    //     );
+    // }
+
+    // randomNumber = tmp;
+        // if (!check.includes(randomNumber)) {
+        //     check.push(randomNumber);
+        //     ansBtn.innerText = choices[randomNumber];
+        //     console.log(randomNumber);
+        //     break;
+        // }
